0adf9e2d20179bd2f88039dedeb90700bde37d0b
Vidit Awasthi <viditawasthi2@gmail.com>
2021-07-31 13:47:35 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var divOutput = document.getElementById("div-output");
+var url = "	https://api.funtranslations.com/translate/braille/unicode.json"
+
+function constructURL(text) {
+    return url + "?text=" + text
+}
+
+function handleError(error) {
+    console.log("error occured", error);
+}
+
+btnTranslate.addEventListener("click", function clickEventHandler() {
+
+    var constructedURL = constructURL(txtInput.value);
+
+    fetch(constructedURL)
+        .then(response => response.json())
+        .then(json => {
+            divOutput.innerText = json.contents.translated
+        })
+        .catch(handleError)
+
+})
