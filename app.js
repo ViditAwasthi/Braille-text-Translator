9c9140565fc602bc8207170551b208df6b82b3a5
Vidit Awasthi <viditawasthi2@gmail.com>
2021-07-31 14:24:47 +0530

    initial commit

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ function constructURL(text) {
 
 function handleError(error) {
     console.log("error occured", error);
+    alert("Error Occurred Please try Later!")
 }
 
 btnTranslate.addEventListener("click", function clickEventHandler() {
